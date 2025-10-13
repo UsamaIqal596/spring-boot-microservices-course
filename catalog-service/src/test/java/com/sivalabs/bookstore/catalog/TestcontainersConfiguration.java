90f0ddeaee90b324115729a5cd680548efc0118f
usama.muhammad <E8ktxTduyw@LYcpWlK960gBr4aLeyEuo.com>
2025-10-13 18:30:52 +0500

    created catalog microservice structure and added build information using maven plugin for actuator/info endpoiint

--- catalog-service/src/test/java/com/sivalabs/bookstore/catalog/TestcontainersConfiguration.java
+++ catalog-service/src/test/java/com/sivalabs/bookstore/catalog/TestcontainersConfiguration.java
@@ -1,5 +1,6 @@
 package com.sivalabs.bookstore.catalog;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +13,15 @@ class TestcontainersConfiguration {
     @Bean
     @ServiceConnection
     PostgreSQLContainer<?> postgresContainer() {
-        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"));
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication
+                .from(CatalogServiceApplication::main)
+                .with(TestcontainersConfiguration.class)
+                .run(args);
+
     }
 
 }
