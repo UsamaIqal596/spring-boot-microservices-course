c69e72789f782dcb4ec95714747e0f1bfa7792f9
usama.muhammad <E8ktxTduyw@LYcpWlK960gBr4aLeyEuo.com>
2025-11-01 02:34:03 +0500

    Fix GitHub Actions branch condition check

--- catalog-service/src/main/java/com/sivalabs/bookstore/catalog/CatalogServiceApplication.java
+++ catalog-service/src/main/java/com/sivalabs/bookstore/catalog/CatalogServiceApplication.java
@@ -5,12 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
-// @EnableConfigurationProperties(ApplicationProperties.class) this is one way other way is
+// @EnableConfigurationProperties(ApplicationProperties.class) this is another way of doing the same thing
 @ConfigurationPropertiesScan
 public class CatalogServiceApplication {
 
     public static void main(String[] args) {
 
-        SpringApplication.run(CatalogServiceApplication.class, args);
+        SpringApplication.run(CatalogServiceApplication.class,args);
     }
 }
