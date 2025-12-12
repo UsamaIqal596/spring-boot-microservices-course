cb49a960cf801ceffd8b939dfc019fec4ee6eb6a
usama.muhammad <E8ktxTduyw@LYcpWlK960gBr4aLeyEuo.com>
2025-12-13 04:19:38 +0500

    fix: uncomment RabbitMQ listener methods for testing purposes

--- order-service/src/main/java/com/sivalabs/bookstore/orders/services/rabitmqtest/RabbitMQListener.java
+++ order-service/src/main/java/com/sivalabs/bookstore/orders/services/rabitmqtest/RabbitMQListener.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class RabbitMQListener {
-
+    // we need to uncomment them just for testing as otherwise they will consume all the messages
     //    @RabbitListener(queues = "${orders.new-orders-queue}")
     //    void handleNewOrder(MyPayload payload) {
     //        System.out.println("Consumed message from queue -> New Order: " + payload.content());
